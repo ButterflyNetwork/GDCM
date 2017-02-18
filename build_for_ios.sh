f37336870346af17c746c61b9d5552a0da9b025c
Krishna Ersson <kersson@butterflynetinc.com>
2017-02-17 22:31:22 -0500

    Adjust visibility settings to match our builds

--- build_for_ios.sh
+++ build_for_ios.sh
@@ -8,14 +8,14 @@ IOS_UNIVERSAL_BUILD_DIR=build-ios-universal
 
 mkdir -p $IPHONEOS_BUILD_DIR
 cd $IPHONEOS_BUILD_DIR
-FLAGS="-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk -miphoneos-version-min=8.0 -arch arm64 -stdlib=libc++"
+FLAGS="-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk -miphoneos-version-min=8.0 -arch arm64 -stdlib=libc++ -fvisibility=hidden"
 cmake .. -DGDCM_BUILD_DOCBOOK_MANPAGES:BOOL=No -DCMAKE_C_FLAGS:String="$FLAGS" -DCMAKE_CXX_FLAGS:String="$FLAGS"
 make -j8
 cd ..
 
 mkdir -p $IPHONESIMULATOR_BUILD_DIR
 cd $IPHONESIMULATOR_BUILD_DIR
-FLAGS="-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk -miphoneos-version-min=8.0 -arch x86_64 -stdlib=libc++"
+FLAGS="-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk -miphoneos-version-min=8.0 -arch x86_64 -stdlib=libc++ -fvisibility=hidden"
 cmake .. -DGDCM_BUILD_DOCBOOK_MANPAGES:BOOL=No -DCMAKE_C_FLAGS:String="$FLAGS" -DCMAKE_CXX_FLAGS:String="$FLAGS"
 make -j8
 cd ..
